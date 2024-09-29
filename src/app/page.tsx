bb78a0412ea081834b400c55b500b0a334a3cf91
Cristhian-shr <81269993+Cristhian-shr@users.noreply.github.com>
2024-09-29 10:18:07 -0300

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Footer from "./components/footerComponents/footer";
 import Header from "./components/headerComponents/header";
 import Products from "./components/productsComponent/products";
 
@@ -8,6 +9,7 @@ export default function Home() {
     <section className="w-full bg-madeJaragua h-full md:h-[100vh]">
       <Header />
       <Products />
+      <Footer />
     </section>
   );
 }
